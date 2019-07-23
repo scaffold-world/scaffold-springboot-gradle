52d28286a452392a81310a8b6cde0fe78a3865be
Fatezhang <762344938@qq.com>
2019-07-23 11:26:35 +0800

    更改job执行相关代码

--- scaffold-route/scaffold-route-operate/src/main/java/com/cms/scaffold/route/operate/job/executer/TaskDisallowConcurrentExecutor.java
+++ scaffold-route/scaffold-route-operate/src/main/java/com/cms/scaffold/route/operate/job/executer/TaskDisallowConcurrentExecutor.java
@@ -1,6 +1,5 @@
 package com.cms.scaffold.route.operate.job.executer;
 
-import com.cms.scaffold.core.spring.SpringContextHolder;
 import com.cms.scaffold.job.jobManager.bo.JobInfoBO;
 import com.cms.scaffold.route.operate.job.utils.InvokeJobUtils;
 import org.quartz.DisallowConcurrentExecution;
@@ -8,17 +7,17 @@ import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 
-/**
- * @Author zhangjiaheng@gmail.com
- * @Description 执行方法 不可并发
- **/
+import javax.annotation.Resource;
+
+/** @Author zhangjiaheng@gmail.com @Description 执行方法 不可并发 */
 @DisallowConcurrentExecution
 public class TaskDisallowConcurrentExecutor implements Job {
 
-    @Override
-    public void execute(JobExecutionContext context) throws JobExecutionException {
-        JobInfoBO jobInfo = (JobInfoBO) context.getMergedJobDataMap().get("JobInfo");
-        InvokeJobUtils invokeJobUtils = SpringContextHolder.getBean(InvokeJobUtils.class);
-        invokeJobUtils.invokMethod(jobInfo);
-    }
+  @Resource private InvokeJobUtils invokeJobUtils;
+
+  @Override
+  public void execute(JobExecutionContext context) throws JobExecutionException {
+    JobInfoBO jobInfo = (JobInfoBO) context.getMergedJobDataMap().get("JobInfo");
+    invokeJobUtils.invokMethod(jobInfo);
+  }
 }
--- scaffold-route/scaffold-route-operate/src/main/java/com/cms/scaffold/route/operate/job/executer/TaskJobExecutor.java
+++ scaffold-route/scaffold-route-operate/src/main/java/com/cms/scaffold/route/operate/job/executer/TaskJobExecutor.java
@@ -1,6 +1,5 @@
 package com.cms.scaffold.route.operate.job.executer;
 
-import com.cms.scaffold.core.spring.SpringContextHolder;
 import com.cms.scaffold.job.jobManager.bo.JobInfoBO;
 import com.cms.scaffold.route.operate.job.utils.InvokeJobUtils;
 import org.quartz.Job;
@@ -8,17 +7,17 @@ import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 import org.springframework.stereotype.Component;
 
-/**
- * @Author zhangjiaheng@gmail.com
- * @Description 执行方法 可并发
- **/
+import javax.annotation.Resource;
+
+/** @Author zhangjiaheng@gmail.com @Description 执行方法 可并发 */
 @Component
 public class TaskJobExecutor implements Job {
 
-    @Override
-    public void execute(JobExecutionContext context) throws JobExecutionException {
-        JobInfoBO jobInfo = (JobInfoBO) context.getMergedJobDataMap().get("JobInfo");
-        InvokeJobUtils invokeJobUtils = SpringContextHolder.getBean(InvokeJobUtils.class);
-        invokeJobUtils.invokMethod(jobInfo);
-    }
+  @Resource private InvokeJobUtils invokeJobUtils;
+
+  @Override
+  public void execute(JobExecutionContext context) throws JobExecutionException {
+    JobInfoBO jobInfo = (JobInfoBO) context.getMergedJobDataMap().get("JobInfo");
+    invokeJobUtils.invokMethod(jobInfo);
+  }
 }
