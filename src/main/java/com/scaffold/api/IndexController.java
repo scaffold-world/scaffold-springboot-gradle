6c711f230071bc7856a8ce07564905a089a36763
Fatezhang <762344938@qq.com>
2020-08-09 22:16:25 +0800

    🐸 Exclude  from jacocoTestCoverageVerification.

--- src/main/java/com/scaffold/api/IndexController.java
+++ src/main/java/com/scaffold/api/IndexController.java
@@ -1,8 +1,14 @@
 package com.scaffold.api;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class IndexController {
 
+    @GetMapping
+    public ResponseEntity<String> helloWorld(){
+        return ResponseEntity.ok().build();
+    }
 }
